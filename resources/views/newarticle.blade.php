77c9b2645e61ad2420a0320a8962cb82eb822bca
ZulfahmiZaid <114953316+ZulfahmiZaid@users.noreply.github.com>
2023-04-20 00:55:24 +0200

    newarticle.blade.php modified

--- resources/views/newarticle.blade.php
+++ resources/views/newarticle.blade.php
@@ -1,18 +1,13 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
 
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title>New Article</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-    <!-- Styles -->
 </head>
-<body class="antialiased">
-<script src="js/newarticle.js"></script>
+<body>
+<script id='script-newarticle' data-token="{{ csrf_token() }}" src="{{asset('js/newarticle.js')}}"></script>
 </body>
 </html>
