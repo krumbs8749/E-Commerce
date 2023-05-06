af3a1d5771a9e0fb4a3ffe633200005e24b270d7
ZulfahmiZaid <114953316+ZulfahmiZaid@users.noreply.github.com>
2023-05-06 17:17:58 +0200

    Aufgabe 1-2

--- public/js/newarticle.js
+++ public/js/newarticle.js
@@ -31,8 +31,10 @@ function populateForm(form){
     })
 
     const button = document.createElement('button')
+    button.id = 'input_submit'
     button.type = 'submit'
     button.innerText = 'Speichern'
+    form = addBreakline(form)
     form.appendChild(button)
 
     return form
@@ -46,9 +48,9 @@ function createForm() {
     let form = document.createElement('form')
     form = populateForm(form)
     form.method = 'POST'
-    form.action = '/articles'
+    form.action = '...'
     // Set submit event listener
-    form.onsubmit = (event) => {
+    /*form.onsubmit = (event) => {
         const formValues = event.target.elements
         if(formValues['art_price'].value <= 0){
             event.preventDefault()
@@ -56,11 +58,47 @@ function createForm() {
             console.log({name: formValues['art_name'].value, price: formValues['art_price'].value, desc: formValues['art_description'].value})
         }
 
-    }
+    }*/
     document.body.appendChild(form)
 
 }
 document.body.onload = () => {
+
     createForm()
+    document.getElementById('input_submit')
+        .addEventListener('click', event =>{
+
+            let art_name = document.querySelector('#art_name').value
+            let art_price = document.querySelector('#art_price').value
+            let art_description = document.querySelector('#art_description').value
+
+            event.preventDefault()
+
+            let formData = new FormData()
+            formData.append("art_name", art_name)
+            formData.append("art_price", art_price)
+            formData.append("art_description", art_description)
+
+            let xhr = new XMLHttpRequest()
+            xhr.open('POST', '/articles')
+            xhr.setRequestHeader("X-Requested-With", "xmlhttprequest");
+            xhr.setRequestHeader('X-CSRF-Token', document.getElementById('script-newarticle').dataset.token)
+
+            let message = document.createElement("p")
+            xhr.onreadystatechange = function () {
+                if(xhr.readyState === 4){
+                    if(xhr.status === 200){
+                        message.innerText = xhr.responseText
+                    }else{
+                        message.innerText = "Server Fehler"
+                    }
+                    document.body.appendChild(message)
+                }
+            }
+
+            xhr.send(formData)
+            return false
+        })
 }
+
 
