5e8f91fc48ba7103960aed91249a6f8dcf01be66
ZulfahmiZaid <114953316+ZulfahmiZaid@users.noreply.github.com>
2023-05-06 18:55:44 +0200

    Aufgabe 8

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -60,4 +60,26 @@ class ArticlesController
 
         return  json_encode($articles);
     }
+
+    public function APIPostArticle(Request $rd){
+        $postData = $rd->post();
+
+        if($postData['art_price'] < 0 || $postData['art_name'] === ""){
+            echo "Fehlerhafte Eingabe";
+            exit();
+        }
+
+        $new_article = new Models\AbArticle;
+
+        $new_article->ab_name = $postData['art_name'];
+        $new_article->ab_price = $postData['art_price'];
+        $new_article->ab_description = $postData['art_description'];
+        $new_article->ab_creator_id = 1;
+        $new_article->ab_createDate = date_format(new \DateTime(), 'Y-m-d H:i:s');
+        $new_article->save();
+
+        $id = Models\AbArticle::query()->select()->max('id');
+
+        return json_encode(["latest_ID" => $id]);
+    }
 }
