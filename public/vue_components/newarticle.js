a50b1a96edcd6ece2a7d28f6765aae0f4a097717
ZulfahmiZaid <114953316+ZulfahmiZaid@users.noreply.github.com>
2023-06-13 17:18:33 +0200

    newArticle to vue

--- public/vue_components/newarticle.js
+++ public/vue_components/newarticle.js
@@ -8,6 +8,8 @@ export default {
             let art_price = document.querySelector('#art_price').value
             let art_description = document.querySelector('#art_description').value
 
+            event.preventDefault()
+
             let formData = new FormData()
             formData.append("art_name", art_name)
             formData.append("art_price", art_price)
@@ -15,6 +17,10 @@ export default {
 
             let xhr = new XMLHttpRequest()
             xhr.open('POST', '/articles')
+            xhr.setRequestHeader("X-Requested-With", "xmlhttprequest");
+            xhr.setRequestHeader('X-CSRF-Token', document.getElementById('csrfToken').content)
+
+            console.log(document.getElementById('csrfToken').content)
 
             let message = document.createElement("p")
             xhr.onreadystatechange = function () {
@@ -29,6 +35,7 @@ export default {
             }
 
             xhr.send(formData)
+            return false
         }
     }
     ,
@@ -45,6 +52,6 @@ export default {
     <input type="text" id="art_description" name="art_description" required><br>
 
     <br>
-    <button type="submit" onclick="sendData">Send</button>
+    <button type="submit" @click="sendData">Send</button>
     </form>`
 }
--- resources/views/vueArticles.blade.php
+++ resources/views/vueArticles.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <script src="https://unpkg.com/vue@next"></script>
+    @vite(['../resources/js/app.js'])
     <title>Vue Article</title>
 
     <!-- Styles -->
